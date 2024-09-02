25eabad172400895c5a86c13c28b1358a59c2676
Silas Owu <owusilasyp00@gmail.com>
2024-09-02 05:25:55 -0700

    feat: import and implement task 1 using 'Of' RxJs Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { of } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Basic_Rxjs-Observables';
+
+  // Cold Observable with Rxjs Creational Operators:
+  constructor(){
+    //Task 1: Creating and Subscribing to an Observable with the 'Of' operator
+    const num = of(4,9,1,6,2).subscribe({
+      next: (value: number) => console.log('Emits ' + value),
+      error: (error) => console.log('Error occur ' + error),
+      complete: () => console.log('Observable subscription Completed')
+    });
+
+
+    //Task 2: Creating and Subscribing to an Observable with the 'Of' operator
+    // const numArray =
+  }
+
 }
